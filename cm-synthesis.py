4549107242270c5c8a1bfa3777c05cc19287e18f
huseyinuri <hngulmez@gmail.com>
2022-11-02 16:55:07 +0300

    fixing
    polynomial functions are united.
    some f-string prints are added

--- cm-synthesis.py
+++ cm-synthesis.py
@@ -7,32 +7,34 @@ from numpy.polynomial import polynomial
 
 """
 =========================
-F: characteristic function
-F(w) = P(w) / D(w)
+C: characteristic function
+C(w) = F(w) / P(w)
 =========================
 S11: reflection function
-S11(w) = P(w) / E(w)
+S11(w) = F(w) / E(w)
 =========================
 S21: transfer function
-S21(w) = D(w) / E(w)e
+S21(w) = P(w) / E(w)e
 =========================
 """
 
-N = 4
-tzs = [1.3217, 1.8082, np.Inf, np.Inf]
+order = 4
+f0 = 3700
+bw = 80
+ftzs_w = [1.80, 1.32]
 
 
-def _populate_tzs(order: int,
-                  finite_tzs: List[float] | None = None) -> List[float]:
-    tzs = [np.Inf]*order
+def _populate_tzs_w(order: int,
+                    ftzs_w: List[float] | None = None) -> List[float]:
+    tzs_w = [np.Inf]*order
 
-    if finite_tzs is None:
-        return tzs
-    if len(finite_tzs) > order:
+    if ftzs_w is None:
+        return tzs_w
+    if len(ftzs_w) > order:
         raise ValueError('Number of finite tz cannot exceed the filter order')
     else:
-        tzs[:len(finite_tzs)] = sorted(finite_tzs)
-    return tzs
+        tzs_w[:len(ftzs_w)] = sorted(ftzs_w)
+    return tzs_w
 
 
 def _calc_fbw(f0: int | float, bw: int | float) -> float:
@@ -42,49 +44,43 @@ def _calc_fbw(f0: int | float, bw: int | float) -> float:
         return 0
 
 
-def _calc_denum(tzs: List[float]):
-    denum_coeffs = [1]
+def _calc_polys_w(order: int, tzs: List[float]):
+    temp = [1]
     for tz in tzs:
-        denum_coeffs = polynomial.polymul(denum_coeffs, [1, -1/tz])
-    return denum_coeffs
+        temp = polynomial.polymul(temp, [1, -1/tz])
+    p_w = temp
 
-
-def _calc_num(order: int, tzs: List[float]):
-
-    num_coeffs = order + 1
     init_0 = [1]
     init_1 = [-1/tzs[0], 1]
-
-    if num_coeffs < 0:
+    if order + 1 < 0:
         raise ValueError("i must be >=0")
-    p0 = init_0
-    if num_coeffs == 0:
-        return p0
-    p1 = init_1
-    for j in range(2, num_coeffs):
+    f0 = init_0
+    if order + 1 == 0:
+        return f0
+    f1 = init_1
+    for j in range(2, order + 1):
         A = ((1-1/(tzs[j-1]**2)) / (1-1/(tzs[j-2]**2)))**0.5
-
-        pnext_1 = polynomial.polymul(p1, [-1/tzs[j-1], 1])
-        pnext_2 = polynomial.polymul(p1, [-1/tzs[j-2], 1])
-        pnext_3 = polynomial.polymul(p0,
+        fnext_1 = polynomial.polymul(f1, [-1/tzs[j-1], 1])
+        fnext_2 = polynomial.polymul(f1, [-1/tzs[j-2], 1])
+        fnext_3 = polynomial.polymul(f0,
                                      polynomial.polypow([1, -1/tzs[j-2]], 2))
+        temp = polynomial.polyadd(fnext_1, A*fnext_2)
+        fnext = polynomial.polysub(temp, A*fnext_3)
+        f0 = f1
+        f1 = fnext
+    f_w = f1
+    return _make_monic(f_w, p_w)
 
-        temp = polynomial.polyadd(pnext_1, A*pnext_2)
-        pnext = polynomial.polysub(temp, A*pnext_3)
 
-        p0 = p1
-        p1 = pnext
-    return p1
-
-
-def _make_monic(Pn, Dn):
-    return (polynomial.polyfromroots(polynomial.polyroots(Pn)),
-            polynomial.polyfromroots(polynomial.polyroots(Dn)))
+def _make_monic(f, p):
+    return (polynomial.polyfromroots(polynomial.polyroots(f)),
+            polynomial.polyfromroots(polynomial.polyroots(p)))
 
 
 def main():
-    Pn = _calc_num(N, tzs)
-    Dn = _calc_denum(tzs)
-    print(Pn)
-    print(Dn)
-    print(_make_monic(Pn, Dn))
+    print(f'Fractional bandwidth-->{_calc_fbw(f0, bw)}')
+    tzs_w = _populate_tzs_w(order, ftzs_w)
+    print(f'TZs in real frequency-->{tzs_w}')
+    f_w, p_w = _calc_polys_w(order, tzs_w)
+    print(f'Monic F(w)-->{f_w}')
+    print(f'Monic P(w)-->{p_w}')
